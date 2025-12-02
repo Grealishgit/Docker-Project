1d0c3908fc4f1d7f3c963e9cafb73018bdaf6007
Eugene <eugyneehunter@gmail.com>
2025-12-02 18:25:25 +0300

    Refactor CSS for layout consistency and improve App component structure

--- react-docker/src/App.tsx
+++ react-docker/src/App.tsx
@@ -1,34 +1,30 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState<number>(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Docker App Running Successfully!</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test files
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div style={{
+      backgroundImage: `url('https://cdn.pixabay.com/photo/2025/11/28/14/39/sunset-9983072_1280.jpg')`,
+      backgroundSize: 'cover',
+      backgroundPosition: 'center',
+      minHeight: '100vh',
+      justifyContent: 'center',
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      color: 'white',
+      textAlign: 'center',
+      padding: '2rem',
+      width: '100%',
+      margin: 0,
+    }}>
+      <h1 style={{ margin: 0, fontSize: '4.2em' }}>Welcome to Docker</h1>
+      <p style={{ margin: 0, fontSize: '2em' }}>Docker App Running Successfully!</p>
+
+    </div>
   )
 }
 
